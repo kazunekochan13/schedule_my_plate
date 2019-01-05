ba03c13d42803ac9983d00319529e92ac79fb6b3
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-05 17:11:57 +0800

    Application successfully runs for first time

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,30 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private String landingPageURL = "/xml/LandingPage.fxml";
+	private String selectionPageURL = "/xml/SelectionPage.fxml";
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage){
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			Parent root = FXMLLoader.load(getClass().getResource(landingPageURL));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public static void main(String[] args) {
---
+++ src/controller/LandingPageController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+/**
+ * 
+ * @author Safwah
+ *
+ */
+public class LandingPageController implements Initializable {
+
+	public void changeSceneToSelectionPage(ActionEvent event) {
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
