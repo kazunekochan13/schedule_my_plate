1e97300042666399e9299b6748dbc1c5af945b20
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-05 17:47:27 +0800

    Connected LandingPage.fxml to coressponding controller

--- src/controller/LandingPageController.java
+++ src/controller/LandingPageController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * 
@@ -14,7 +20,16 @@ import javafx.fxml.Initializable;
 public class LandingPageController implements Initializable {
 
 	public void changeSceneToSelectionPage(ActionEvent event) {
-		
+		try {
+			Parent selectionPageParent = FXMLLoader.load(getClass().getResource("/xml/SelectionPage.fxml"));
+			Scene selectionPageScene = new Scene(selectionPageParent);
+			// this line gets the stage information
+			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+			window.setScene(selectionPageScene);
+			window.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
